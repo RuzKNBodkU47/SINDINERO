fcf1260543a17ade29cb670dfae660dc52c146a8
elcuama <65052112+elcuama@users.noreply.github.com>
2020-05-26 23:25:55 -0500

    Add files via upload

---
+++ Inicio.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Inicio</title>
+	<link rel="stylesheet" type="text/css" href="css/registro.css">
+	<link rel="stylesheet" type="text/css" href="css/menu.css">
+	<link rel="stylesheet" type="text/css" href="css/header.css">
+</head>
+<body>
+	<header>
+			<nav class="Navegacion">
+		 	<ul class="menu">
+		 		<li><a href="">Inicio</a></li>
+		 		<li><a href="Inicio.php/.about_us">Acerca de nosotros</a></li>
+		 		<li><a href="login.php">Iniciar Sesión</a></li>
+		 	</ul>
+			 </nav>
+		<div class="contenido">
+		 	<div class="texto_header">
+		 		<br>
+		 		<h1>Sin dinero</h1>
+		 		<br>
+		 		<p align="justify">
+		 			Bienvenidos a Sin Dinero, la aplicación web para estudiantes y al público en general, que permitirá al usuario realizar deliciosas recetas con un mínimo presupuesto.
+		 		</p>
+		 		<br>
+		 		<br>
+		 		<a href="Registro.php">Registrate</a>
+		 	</div> 
+		 	<div class="Image">
+		 			<img src="img/chef.svg">
+		 	</div>
+		 </div>
+	</header>
+
+	
+	<section id="Abous_us">
+		<div class="context">
+			<h2>Sobre nosotros</h2>
+			<p align="justify">
+				Somos un grupo de compañeros y amigos que localizamos la necesidad de estudiantes y personas que contamos de un ingreso justo y queremos cambiar hábitos de alimentación para un estilo de vida sano. 
+			</p>
+		</div>
+		<img src="img/programer.svg">
+	</section>
+</body>
+</html>
